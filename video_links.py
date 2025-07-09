d981ffb1fb7dfac7b69405b76b3c1ddde14dd3df
rmayank-24 <137880416+rmayank-24@users.noreply.github.com>
2025-07-09 22:31:31 +0530

    Add files via upload

---
+++ video_links.py
@@ -0,0 +1,32 @@
+# video_links.py
+
+video_map = {
+  "Devotion": [
+    {
+      "title": "Why fit in..?",
+      "url": "https://video-cdn.socialverseapp.com/michael_5f68cfbe-d0aa-4346-ad84-6536d5191873.mp4"
+    },
+    {
+      "title": "Transcending Singularity",
+      "url": "https://video-cdn.socialverseapp.com/michael_54a0ad4d-695c-49a0-bbb3-25bf81d051f9.mp4"
+    }
+  ],
+  "Prayer": [
+    {
+      "title": "Direct your energy on the right things",
+      "url": "https://video-cdn.socialverseapp.com/michael_4901cc10-76ef-4dda-b198-1c6699b19043.mp4"
+    }
+  ],
+  "Meditation": [
+    {
+      "title": "WE ARE MADE OF STARS",
+      "url": "https://video-cdn.socialverseapp.com/michael_1085c54c-26e8-4691-8558-f618f7b5732f.mp4"
+    }
+  ],
+  "Accountability": [
+    {
+      "title": "Consistency",
+      "url": "https://video-cdn.socialverseapp.com/afrobeezy_19213d62-2e9c-4569-ae5c-569b90759ace.mp4"
+    }
+  ]
+}
